b3095d97b24346a135208c2974c5f0cadbcbc74c
ankitsejwal <ankit@sejw.al>
2018-06-14 00:45:37 +1000

    step4 - done

--- moonhack-2018/main.py
+++ moonhack-2018/main.py
@@ -2,4 +2,4 @@ from classes import *
 
 targetPlanet = input("Type the name of a planet: ")
 # print(targetPlanet)
-# print(ourSolarSystem.getPlanet('Jupiter'))
+print(ourSolarSystem.getPlanet(targetPlanet))
