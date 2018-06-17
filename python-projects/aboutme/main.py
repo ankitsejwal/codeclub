b934c8b45f615b1b4dc8652496c877b3a624c827
ankitsejwal <ankit@sejw.al>
2018-06-18 00:40:32 +1000

    challenge dog age

--- python-projects/aboutme/main.py
+++ python-projects/aboutme/main.py
@@ -17,3 +17,12 @@ Here's a cow face:
 born = int(input('What year were you born?: '))
 age = 2025 - born
 print("In the year 2025 you'll be", age ,"years old." )
+
+# challege - dog age
+age = int(input('What is your age?:'))
+dog_age = age * 7
+print("If you were a dog, you'd be", dog_age,"!!")
+print('''
+o____
+ ||||
+''')
