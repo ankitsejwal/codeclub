692c94ff3f81773e18631d86e7c64da24d46bea0
ankitsejwal <ankit@sejw.al>
2018-06-18 00:58:47 +1000

    step2

--- python-projects/rock-paper-scissors/main.py
+++ python-projects/rock-paper-scissors/main.py
@@ -2,5 +2,15 @@
 
 from random import randint
 
-player = input('rock (r), paper (p) or scissor (s)?')
-print(player, 'vs')
+player = input('rock (r), paper (p) or scissor (s)? ')
+
+chosen = randint(1,3)
+if chosen == 1:
+    computer = 'r'
+elif chosen == 2:
+    computer = 'p'
+else:
+    computer = 's'
+
+print(player, 'vs', computer)
+
