3cecb8b86279a8e70dda696c8c6aa8c6179e9291
ankitsejwal <ankit@sejw.al>
2018-06-19 01:07:14 +1000

    step4

--- python-projects/team-chooser/main.py
+++ python-projects/team-chooser/main.py
@@ -2,25 +2,32 @@
 
 from random import choice
 
-players = ["Harry", "Hermione", "Jue", "Patrick"]
+# players = ["Harry", "Hermione", "Jue", "Patrick"]
+# print(players)
+
+
+# while len(players) > 0:
+
+#     playerA = choice(players)
+#     print(playerA)
+#     teamA.append(playerA)
+#     players.remove(playerA)
+#     print('Players left: ', players)
+
+#     playerB = choice(players)
+#     print(playerB)
+#     teamB.append(playerB)
+#     players.remove(playerB)
+#     print('Players left: ', players)
+
+# print(teamA)
+# print(teamB)
+
+players = []
+file = open('players.txt', 'r')
+players = file.read().splitlines()
 print(players)
 
 teamA = []
 teamB = []
 
-while len(players) > 0:
-
-    playerA = choice(players)
-    print(playerA)
-    teamA.append(playerA)
-    players.remove(playerA)
-    print('Players left: ', players)
-
-    playerB = choice(players)
-    print(playerB)
-    teamB.append(playerB)
-    players.remove(playerB)
-    print('Players left: ', players)
-
-print(teamA)
-print(teamB)
