5a3c8d85e09dd297e965541c1270756f08caa568
ankitsejwal <ankit@sejw.al>
2018-06-19 01:27:01 +1000

    challege new team

--- python-projects/team-chooser/main.py
+++ python-projects/team-chooser/main.py
@@ -6,8 +6,10 @@ players = []
 file = open('players.txt', 'r')
 players = file.read().splitlines()
 
+# setting up empty lists
 teamA = []
 teamB = []
+teamC = []
 
 while len(players) > 0:
 
@@ -26,14 +28,27 @@ while len(players) > 0:
     players.remove(playerB)
     print('Players left: ', players)
 
+    if players == []:
+        break
+
+    playerC = choice(players)
+    teamC.append(playerC)
+    players.remove(playerC)
+    
+# Saving results in text files
 teamA_file = open('teamA.txt', 'w')
 for player in teamA:
     teamA_file.writelines(player + '\n')
-
-print(teamA)
 
 teamB_file = open('teamB.txt', 'w')
 for player in teamB:
     teamB_file.writelines(player + '\n')
 
-print(teamB)
+teamC_file = open('teamC.txt', 'w')
+for player in teamC:
+    teamC_file.writelines(player + '\n')
+
+# Printing team results
+print('TeamA: ', teamA)
+print('TeamB: ', teamB)
+print('TeamC: ', teamC)
